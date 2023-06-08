f9dd51d0af1106b34072fa55ceb863f6b40dc737
Michael Ulguim <michaelulguim@gmail.com>
2023-06-08 15:45:56 -0300

    add events

---
+++ src/handler.events.ts
@@ -0,0 +1,13 @@
+import { EventEmitter } from "node:events";
+import { HandlerEventCallback, HandlerEventTypes } from "./types";
+
+export class HandlerEventsEmitter extends EventEmitter {
+  dispatchEvent(event: HandlerEventTypes, data: any) {
+    this.emit(event, data);
+  }
+
+  on(event: HandlerEventTypes, callback: HandlerEventCallback) {
+    super.on(event, (data) => (callback ? callback(data) : null));
+    return this;
+  }
+}
